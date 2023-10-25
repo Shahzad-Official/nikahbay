e9a78337ceb4decc2380d5ecca2853209ce4d1b0
Muhammad Waris <m.waris43214@gmail.com>
2023-10-25 21:43:39 +0500

    update

--- lib/controllers/theme.dart
+++ lib/controllers/theme.dart
@@ -5,21 +5,30 @@ class Themes {
   final lightTheme = ThemeData.light().copyWith(
     primaryColor: Colors.blueGrey[300],
     brightness: Brightness.light,
+    scaffoldBackgroundColor: Colors.white,
     appBarTheme: AppBarTheme(
+      iconTheme: const IconThemeData(
+        color: Color(0xff21262D),
+      ),
+      color: Colors.white,
+      elevation: 0,
       centerTitle: true,
       titleTextStyle: GoogleFonts.roboto(
         fontSize: 18,
         fontWeight: FontWeight.w600,
-        color: Colors.white,
+        color: const Color(0xff21262D),
       ),
     ),
-   
   );
 
   final darkTheme = ThemeData.dark().copyWith(
+    iconTheme: IconThemeData(color: Colors.white),
     primaryColor: Colors.blueGrey[300],
     brightness: Brightness.light,
+    scaffoldBackgroundColor: const Color(0xff21262D),
     appBarTheme: AppBarTheme(
+      color: const Color(0xff21262D),
+      elevation: 0,
       centerTitle: true,
       titleTextStyle: GoogleFonts.roboto(
         fontSize: 18,
@@ -27,6 +36,5 @@ class Themes {
         color: Colors.white,
       ),
     ),
-   
   );
 }
