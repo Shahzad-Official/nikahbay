a797dfffc570947acff47ec207f3ad23cafe41f2
Muhammad Waris <m.waris43214@gmail.com>
2023-11-08 21:43:09 +0500

    update

--- lib/views/profile_view/settings/verification/success_screen.dart
+++ lib/views/profile_view/settings/verification/success_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:nikahbay/constants/app_colors.dart';
+import 'package:nikahbay/constants/app_spacing.dart';
 import 'package:nikahbay/utils/app_navigation.dart';
 import 'package:nikahbay/views/main_page/main_page.dart';
 import 'package:nikahbay/widgets/app_button.dart';
-
 import 'package:nikahbay/widgets/app_text.dart';
 
 class SuccessScreen extends StatelessWidget {
@@ -11,46 +12,64 @@ class SuccessScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            "assets/images/bg.jpg",
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity,
-          ),
-          Column(
-            children: [
-              const Spacer(
-                flex: 2,
+      appBar: AppBar(
+        leading: InkWell(
+            onTap: () {
+              AppNavigation.offAll(context, nextPage: const MainPage());
+            },
+            child: const Icon(Icons.arrow_back_ios_new_rounded)),
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: AppSpacing.defaultPadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(
+              flex: 2,
+            ),
+            Image.asset(
+              "assets/images/success.png",
+              width: MediaQuery.of(context).size.width * 0.50,
+            ),
+            AppSpacing.heigthSpace30,
+            const AppText(
+              text: "Thank You!",
+              color: AppColors.secondaryColor,
+              fontSize: 40,
+            ),
+            const AppText(
+              text: "Request Submitted Successfully",
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+            ),
+            AppSpacing.heigthSpace10,
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.80,
+              child: const AppText(
+                text:
+                    "Thank you for reaching out! Your message is valuable to us. Our team is dedicated to providing you with a prompt response, typically within two to three business days.",
+                fontStyle: FontStyle.italic,
+                textAlign: TextAlign.center,
               ),
-              Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.7),
-                  borderRadius: BorderRadius.circular(15),
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AppButton(
+                  text: "Go to HomePage",
+                  width: MediaQuery.of(context).size.width * 0.80,
+                  onTap: () {
+                    AppNavigation.to(context, nextPage: const MainPage());
+                  },
+                  height: 55,
                 ),
-                child: const AppText(
-                  text: "We will let you know in 2-3 days after verification.",
-                  color: Colors.white,
-                  fontSize: 20,
-                  textAlign: TextAlign.center,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              Spacer(),
-              AppButton(
-                text: "Go to HomePage",
-                width: MediaQuery.of(context).size.width * 0.80,
-                onTap: () {
-                  AppNavigation.to(context, nextPage: const MainPage());
-                },
-                height: 55,
-              ),
-              const Spacer(),
-            ],
-          ),
-        ],
+              ],
+            ),
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }
